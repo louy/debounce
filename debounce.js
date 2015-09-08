3ff1b2584dcc66637250677f8418089cf52e76e6
Louy <me@l0uy.com>
2015-09-08 19:20:10 +0300

    development files

--- debounce.js
+++ debounce.js
@@ -2,92 +2,100 @@
  * debounce 0.2.0 by Louy Alakkad
  * https://github.com/louy/debounce/
  */
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.debounce = factory();
+(function(root, factory) {
+  /* global define */
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.debounce = factory();
+  }
+}(this, function() {
+
+  /**
+   * debounce( id, delay, fn );
+   *   run fn after delay, it will cancel and override (debounce)
+   *   previous one with same id.
+   * debounce( id, true );
+   *   run fn with specified id instantly, cancel specified delay earlier.
+   * debounce( id );
+   *   cancel fn with specified delay.
+   *
+   * fn can return any of the following:
+   * - true(ish) : re-schedule the same fn after the same delay.
+   * - number    : re-schedule the same fn after (number) delay.
+   * - false(ish): don't re-schedule anything.
+   *
+   * if fn overrides the same id (i.e. calls debounce() with the same id) then
+   *   return value will be ignored.
+   */
+
+  var version = '0.2.0',
+  s = {};
+
+  var debounce = function(id, delay, fn) {
+
+    if (id && !fn) {
+      if (s[id]) {
+        if (delay === true) { // call instantly
+          clearTimeout(s[id].id);
+          s[id].cb();
+        } else { // cancel
+          clearTimeout(s[id].id);
+          s[id] = false;
+        }
+      }
+
+      return;
     }
-}(this, function () {
 
-	/**
-	 * debounce( id, delay, fn ); // run fn after delay, it will cancel and override (debounce) previous one with same id.
-	 * debounce( id, true ); // run fn with specified id instantly, cancel specified delay earlier.
-	 * debounce( id ); // cancel fn with specified delay.
-	 *
-	 * fn can return any of the following:
-	 * - true(ish) : re-schedule the same fn after the same delay.
-	 * - number    : re-schedule the same fn after (number) delay.
-	 * - false(ish): don't re-schedule anything.
-	 *
-	 * if fn overrides the same id (i.e. calls debounce() with the same id) then return value will be ignored.
-	 */
+    if (s[id]) {
+      clearTimeout(s[id].id);
+      s[id] = false;
+    }
 
-	var version = '0.2.0',
-		s = {};
+    s[id] = {
+      fn: fn,
+      delay: delay,
+      run: 0,
+      set: function() {
+        return (this.id = setTimeout(this.cb, this.delay));
+      },
+    };
 
-	var debounce = function( id, delay, fn ) {
+    s[id].cb = function() {
+      var prevID = s[id].id,
+      exec = fn(s[id].run);
+      ++s[id].run;
 
-		if( id && !fn ) {
-			if( s[id] ) {
-				if( delay === true ) { // call instantly
-					clearTimeout( s[id].id );
-					s[id].cb();
-				} else { // cancel
-					clearTimeout( s[id].id );
-					s[id] = false;
-				}
-			}
-			return;
-		}
+      if (prevID !== s[id].id) {
+        // detected override, cancel
+        return;
+      }
 
-		if( s[id] ) {
-			clearTimeout( s[id].id );
-			s[id] = false;
-		}
+      if (typeof exec === 'number') {
+        // re-schedule after (number) ms.
+        s[id].delay = exec;
+        exec = true;
+      }
 
-		s[id] = {
-			fn: fn,
-			delay: delay,
-			run: 0,
-			set: function() {
-				return ( this.id = setTimeout( this.cb, this.delay ) );
-			},
-		};
+      if (!!exec) {
+        // reschedule
+        s[id].set();
+      } else {
+        // remove
+        delete s[id];
+      }
+    };
 
-		s[id].cb = function() {
-			var prevID = s[id].id,
-				exec = fn(s[id].run);
-				++ s[id].run;
+    s[id].set();
+  };
 
-			if( prevID != s[id].id ) {
-				// detected override, cancel
-				return;
-			}
+  debounce.version = version;
+  debounce.isSet = function(id) {
+    return !!s[id];
+  };
 
-			if( typeof exec === 'number' ) {
-				// re-schedule after (number) ms.
-				s[id].delay = exec;
-				exec = true;
-			}
-
-			if( !!exec ) {
-				// reschedule
-				s[id].set();
-			} else {
-				// remove
-				delete s[id];
-			}
-		};
-		s[id].set();
-	};
-
-	debounce.version = version;
-	debounce.isSet = function( id ) {
-		return !!s[id];
-	};
-
-	return debounce;
-} ) );
+  return debounce;
+}));
